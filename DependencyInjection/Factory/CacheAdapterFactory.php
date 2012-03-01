d547add0788f3bee489c54a8c2642a90343e4cc2
Robin van der Vleuten <robinvdvleuten@gmail.com>
2012-03-01 17:13:11 +0100

    CacheAdapterFactory created

---
+++ DependencyInjection/Factory/CacheAdapterFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Knp\Bundle\GaufretteBundle\DependencyInjection\Factory;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Cache adapter factory
+ *
+ * @author Robin van der Vleuten <robinvdvleuten@gmail.com>
+ */
+class CacheAdapterFactory implements AdapterFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+    	$container
+            ->setDefinition($id, new DefinitionDecorator('knp_gaufrette.adapter.cache'))
+            ->addArgument(new Reference('gaufrette.' . $config['source'] . '_adapter'))
+            ->addArgument(new Reference('gaufrette.' . $config['cache'] . '_adapter'))
+            ->addArgument($config['ttl'])
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getKey()
+    {
+        return 'cache';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('source')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('cache')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('ttl')->defaultValue(0)->end()
+            ->end()
+        ;
+    }
+}
