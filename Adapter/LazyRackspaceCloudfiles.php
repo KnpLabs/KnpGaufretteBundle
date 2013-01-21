99586c1f79b94a1878e46305dadcbcadcbbcb8d6
lmammino <lmammino@oryzone.com>
2013-01-21 17:02:22 +0100

    Improved lazy loading

--- Adapter/LazyRackspaceCloudfiles.php
+++ Adapter/LazyRackspaceCloudfiles.php
@@ -9,13 +9,34 @@ use \CF_Container as RackspaceContainer,
 use Gaufrette\Adapter\RackspaceCloudfiles;
 
 /**
- * Rackspace cloudfiles adapter
+ * Rackspace cloudfiles adapter (based on the default gaufrette rackspace adapter) that issues authentication and
+ * initializes the container only when needed to.
  *
  * @package GaufretteBundle
  * @author  Luciano Mammino <lmammino@oryzone.com>
  */
 class LazyRackspaceCloudfiles extends RackspaceCloudfiles
 {
+    /**
+     * @var \CF_Authentication $authentication
+     */
+    protected $authentication;
+
+    /**
+     * @var string $containerName
+     */
+    protected $containerName;
+
+    /**
+     * @var bool $createContainer
+     */
+    protected $createContainer;
+
+    /**
+     * @var bool $initialized
+     */
+    protected $initialized = FALSE;
+
     /**
      * Constructor.
      * Creates a new Rackspace adapter starting from a rackspace authentication instance and a container name
@@ -26,18 +47,84 @@ class LazyRackspaceCloudfiles extends RackspaceCloudfiles
      */
     public function __construct(RackspaceAuthentication $authentication, $containerName, $createContainer = FALSE)
     {
-        if(!$authentication->authenticated())
-            $authentication->authenticate();
-
-        $conn = new RackspaceConnection($authentication);
-
-        $container = NULL;
-        if($createContainer)
-            $container = $conn->create_container($containerName);
-        else
-            $container = $conn->get_container($containerName);
-
-        parent::__construct($container);
+        $this->authentication = $authentication;
+        $this->containerName = $containerName;
+        $this->createContainer = $createContainer;
     }
 
+    /**
+     * Initializes the container
+     */
+    protected function initialize()
+    {
+        if(!$this->initialized)
+        {
+            if(!$this->authentication->authenticated())
+                $this->authentication->authenticate();
+
+            $conn = new RackspaceConnection($this->authentication);
+
+            $container = NULL;
+            if($this->createContainer)
+                $this->container = $conn->create_container($this->containerName);
+            else
+                $this->container = $conn->get_container($this->containerName);
+
+            $this->initialized = TRUE;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function read($key)
+    {
+        $this->initialize();
+        return parent::read($key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function write($key, $content, array $metadata = null)
+    {
+        $this->initialize();
+        return parent::write($key, $content, $metadata);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function exists($key)
+    {
+        $this->initialize();
+        return parent::exists($key);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function keys()
+    {
+        $this->initialize();
+        return parent::keys();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function checksum($key)
+    {
+        $this->initialize();
+        return parent::keys();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete($key)
+    {
+        $this->initialize();
+        return parent::delete($key);
+    }
 }
