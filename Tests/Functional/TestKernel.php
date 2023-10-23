e320100e6a32daab2f4f204e3ea63fcbe6c1b0a5
muchafm <47305202+muchafm@users.noreply.github.com>
2023-10-23 17:30:26 +0200

    fix: typehint on TestKernel.php

--- Tests/Functional/TestKernel.php
+++ Tests/Functional/TestKernel.php
@@ -19,7 +19,7 @@ class TestKernel extends Kernel
         );
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader): mixed
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__.'/Resources/config/config_'.$this->getEnvironment().'.yml');
     }
